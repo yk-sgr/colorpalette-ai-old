a79a76bab8c2f5c83fc3b35a091fb8ff6d029a60
Yannick Seeger <yannick.sgr@gmail.com>
2023-04-27 23:40:14 +0200

    fix: home colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import {Button} from "@/components/ui/button";
 import Link from "next/link";
-import {env} from '@/env';
+import {env} from "@/env";
 
 export default function IndexPage() {
   return (
@@ -65,30 +65,30 @@ function Pricing() {
   ];
 
   return (
-    <section className='relative py-14'>
+    <section className="relative py-14">
       <div className="absolute top-0 h-[521px] w-full bg-gradient-to-b from-primary/10 to-background"></div>
       <div className="mx-auto max-w-screen-xl sm:px-4 md:px-8">
-        <div className='relative mx-auto max-w-xl space-y-3 px-4 sm:px-0 sm:text-center'>
+        <div className="container relative mx-auto max-w-xl space-y-3 sm:px-0 sm:text-center">
           <h3 className="font-semibold text-primary">
             Pricing
           </h3>
-          <p className='text-3xl font-semibold sm:text-4xl'>
+          <p className="text-3xl font-semibold sm:text-4xl">
             Affordable and Transparent Pricing
           </p>
-          <div className='max-w-xl text-muted-foreground'>
+          <div className="max-w-xl text-muted-foreground">
             <p>
               Our all-in-one plan caters to both personal and small business needs, offering the ideal balance of
               features and affordability.
             </p>
           </div>
         </div>
-        <div className='mt-16 justify-center sm:flex'>
+        <div className="mt-16 justify-center sm:flex container mx-auto">
           {
             plans.map((item, idx) => (
               <div key={idx}
-                   className={`relative mt-6 flex flex-1 flex-col items-stretch sm:mt-0 sm:max-w-md sm:rounded-lg ${item.isMostPop ? "bg-white shadow-lg dark:bg-accent sm:border" : ""}`}>
+                   className={`relative mt-6 flex flex-1 flex-col items-stretch sm:mt-0 sm:max-w-md rounded-lg ${item.isMostPop ? "bg-accent shadow-lg sm:border" : ""}`}>
                 <div className="space-y-4 border-b p-4 py-8 md:p-8">
-                  <span className='font-medium text-primary'>
+                  <span className="font-medium text-primary">
                       {item.name}
                   </span>
                   <div className={`text-3xl font-semibold text-accent-foreground`}>
@@ -110,22 +110,22 @@ function Pricing() {
                     </a>
                     )}
                 </div>
-                <ul className='space-y-3 p-4 py-8 md:p-8'>
+                <ul className="space-y-3 p-4 py-8 md:p-8">
                   <li className="pb-2 font-medium">
                     <p className={""}>Features</p>
                   </li>
                   {
                     item.features.map((featureItem, idx) => (
-                      <li key={idx} className='flex items-center gap-5 text-muted-foreground'>
+                      <li key={idx} className="flex items-center gap-5 text-muted-foreground">
                         <svg
-                          xmlns='http://www.w3.org/2000/svg'
-                          className='h-5 w-5 text-primary'
-                          viewBox='0 0 20 20'
-                          fill='currentColor'>
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-5 w-5 text-primary"
+                          viewBox="0 0 20 20"
+                          fill="currentColor">
                           <path
-                            fillRule='evenodd'
-                            d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z'
-                            clipRule='evenodd'></path>
+                            fillRule="evenodd"
+                            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                            clipRule="evenodd"></path>
                         </svg>
                         {featureItem}
                       </li>
