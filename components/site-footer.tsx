6b4768fa81e7b52dd03758e47bcc9b3c926aad0d
Yannick Seeger <yannick.sgr@gmail.com>
2023-04-27 11:34:50 +0200

    fix: footer responsiveness

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -4,9 +4,9 @@ import {Button} from '@/components/ui/button';
 export default function SiteFooter() {
   return (
     <footer className="container mb-4 mt-8 rounded-lg">
-      <div className="mx-auto md:flex md:items-center md:justify-between">
+      <div className="mx-auto flex flex-col-reverse items-center justify-center sm:flex-row sm:justify-between">
         <a className="text-sm sm:text-center">© 2023 ColorPaletteAI.</a>
-        <ul className="mt-3 flex flex-wrap items-center text-sm font-medium sm:mt-0">
+        <ul className="mt-3 flex flex-wrap items-center text-sm font-medium sm:mt-0 justify-center">
           <li>
             <Link href={"/privacy-policy"}>
               <Button variant={"ghost"}>Privacy Policy</Button>
